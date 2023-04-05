0391340565e2d9a3d71a681c17f42f01c05d39c8
kanakweb <kanakwebdev@gmail.com>
2023-04-05 16:48:00 +0600

    this code start now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Switch,Link,router} from 'react-router-dom';
+import Register from './Component/Register/register'
+import Login from './Component/Login/login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="">
+    <Link to={'/login'}>Login</Link>
+    <Link to={'/register'}>Register</Link>
     </div>
+    <hr/>
+    <Register/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Component/Register/register.js
@@ -0,0 +1,11 @@
+function Register() {
+    return(
+       <form action="">
+
+    <h2>Register</h2>
+    <input type="email" placeholder="enter your email" />
+    inpu
+        </form> 
+    );
+}
+export default Register;
